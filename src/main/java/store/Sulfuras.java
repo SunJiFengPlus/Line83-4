fa428d1f5caf032ddfd8c2b740f1479ca2097fca
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-03-31 10:18:57 +0800

    修复魔锤逻辑

--- src/main/java/store/Sulfuras.java
+++ src/main/java/store/Sulfuras.java
@@ -14,10 +14,6 @@ public class Sulfuras extends Item {
      */
     @Override
     public void update() {
-        if (sellIn < 0) {
-            if (value > 0) {
-                value = value - 1;
-            }
-        }
+
     }
 }
