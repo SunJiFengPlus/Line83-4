3ebba7b13d72bd10c5ad96505e33fcb20099dea6
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-06-07 11:32:17 +0800

    查看测试用例覆盖程度

--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -1,11 +1,17 @@
 package store;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
 public class StoreTest{
     @Test
     public void test() {
-        
+        CombinationApprovals.verifyAllCombinations(
+                this::updateValue,
+                new String[]{"foo"},
+                new Integer[]{0},
+                new Integer[]{0}
+        );
     }
     
     public Item updateValue(String name, int sellIn, int value) {
