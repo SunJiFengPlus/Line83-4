b2acd0268d5e2f5678e4f46fbf2fca6e3b9548d7
SunJiFeng <sunjifeng@sfmail.sf-express.com>
2021-06-10 15:37:39 +0800

    提取方法

--- share.md
+++ share.md
@@ -65,3 +65,9 @@ if (name.equalsTo("Sulfuras")) {
     // do something
 }
 ```
+
+按照上面的思路来, 要把代码搞成 if、else if、else 的结构
+> 提取方法(Extract Moth): Option + Command + M
+
+把核心逻辑提取到一个方法中, 随便命名一下, 再把代码格式调整到预期的格式<br>
+运行测试用例, 通过, 不错, 没有影响代码的运行效果, 虽然代码变的更烂了😂
--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -16,48 +16,60 @@ public class Store {
     // Please don't modify the signature of this method.
     public void updateValue() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Wine") && !item.name.equals("Show Ticket")) {
-                if (item.value > 0) {
-                    if (!item.name.equals("Sulfuras")) {
+            if (item.name.equals("Aged Wine")) {
+                foo(item);
+            } else if (item.name.equals("Show Ticket")) {
+                foo(item);
+            } else if (item.name.equals("Sulfuras")) {
+                foo(item);
+            } else {
+                foo(item);
+            }
+        }
+    }
+    
+    private void foo(Item item) {
+        if (!item.name.equals("Aged Wine") && !item.name.equals("Show Ticket")) {
+            if (item.value > 0) {
+                if (!item.name.equals("Sulfuras")) {
+                    item.value = item.value - 1;
+                }
+            }
+        } else {
+            if (item.value < 50) {
+                item.value = item.value + 1;
+            
+                if (item.name.equals("Show Ticket")) {
+                    if (item.sellIn < 11) {
+                        if (item.value < 50) {
+                            item.value = item.value + 1;
+                        }
+                    }
+                
+                    if (item.sellIn < 6) {
+                        if (item.value < 50) {
+                            item.value = item.value + 1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        if (!item.name.equals("Sulfuras")) {
+            item.sellIn = item.sellIn - 1;
+        }
+        
+        if (item.sellIn < 0) {
+            if (!item.name.equals("Aged Wine")) {
+                if (!item.name.equals("Show Ticket")) {
+                    if (item.value > 0) {
                         item.value = item.value - 1;
                     }
+                } else {
+                    item.value = 0;
                 }
             } else {
-                if (item.value < 50) {
-                    item.value = item.value + 1;
-                
-                    if (item.name.equals("Show Ticket")) {
-                        if (item.sellIn < 11) {
-                            if (item.value < 50) {
-                                item.value = item.value + 1;
-                            }
-                        }
-                    
-                        if (item.sellIn < 6) {
-                            if (item.value < 50) {
-                                item.value = item.value + 1;
-                            }
-                        }
-                    }
-                }
-            }
-        
-            if (!item.name.equals("Sulfuras")) {
-                item.sellIn = item.sellIn - 1;
-            }
-        
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Wine")) {
-                    if (!item.name.equals("Show Ticket")) {
-                        if (item.value > 0) {
-                            item.value = item.value - 1;
-                        }
-                    } else {
-                        item.value = 0;
-                    }
-                } else {
-                    item.value = item.value + 1;
-                }
+                item.value = item.value + 1;
             }
         }
     }
